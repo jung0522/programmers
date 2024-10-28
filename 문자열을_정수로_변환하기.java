ebea1f744e5f7bda3dc333d91cc73ea57dc96fcd
jung0522 <jjy1111202@naver.com>
2024-10-29 02:48:26 +0900

    배일의 길이를 이용한 문제들/타입 변환

---
+++ 문자열을_정수로_변환하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String n_str) {
+        int answer = 0;
+        answer = Integer.parseInt(n_str); // String.valueOf() 숫자 -> 문자열
+        return answer;
+    }
+}
